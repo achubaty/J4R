d8fc01a975568a6e3d13b1f08dca85f6a4e8863c
donenrique <donenrique@c5ebbc72-d003-4893-bbcb-3580462f3885>
2019-01-14 13:18:37 +0000

    Refactoring unit test in J4R

--- tests/testthat/testPerformances.R
+++ tests/testthat/testPerformances.R
@@ -4,52 +4,49 @@
 
 context("Testing performance")
 
-library(rJava)
+#library(rJava)
 library(J4R)
 
 connectToJava()
-.jinit()
+#.jinit()
 
 createObjects <- function(n) {
   elapsedTimeJ4R <- c()
-  elapsedTimeRJava <- c()
+#  elapsedTimeRJava <- c()
 
   for (iter in 1:100) {
     start <- Sys.time()
     myArrayListJ4R <- createJavaObject("java.util.ArrayList", rep(as.integer(10),n))
     elapsedTimeJ4R <- c(elapsedTimeJ4R, as.numeric(Sys.time() - start, units="secs"))
 
-    start <- Sys.time()
-    output <- list()
-    for (i in 1:n) {
-      output[[i]] <- .jnew("java/util/ArrayList")
-    }
-    elapsedTimeRJava <- c(elapsedTimeRJava, as.numeric(Sys.time() - start, units="secs"))
+    # start <- Sys.time()
+    # output <- list()
+    # for (i in 1:n) {
+    #   output[[i]] <- .jnew("java/util/ArrayList")
+    # }
+    # elapsedTimeRJava <- c(elapsedTimeRJava, as.numeric(Sys.time() - start, units="secs"))
   }
 
-  return(c(mean(elapsedTimeJ4R), mean(elapsedTimeRJava)))
+  return(c(mean(elapsedTimeJ4R)))
 }
 
-#### Average time to create 1, 5, 10, and 50 objects in J4R and rJava
+#### Average time to create 1, 5, 10, and 50 arraylist in J4R
 
-elapsedTimes <- createObjects(1)
-test_that("rJava is faster for the instantiation of 1 object", {
-  expect_equal(elapsedTimes[1] > elapsedTimes[2], TRUE)
-})
+nbObjects <- c(1,5,10,50,100,200)
 
-elapsedTimes <- createObjects(5)
-test_that("rJava is faster for the instantiation of 5 objects", {
-  expect_equal(elapsedTimes[1] < elapsedTimes[2], TRUE)
-})
+elapsedTimes <- c()
+for (nbObj in nbObjects) {
+  elapsedTimes <- c(elapsedTimes, createObjects(nbObj))
+}
 
-elapsedTimes <- createObjects(10)
-test_that("rJava is faster for the instantiation of 10 objects", {
-  expect_equal(elapsedTimes[1] < elapsedTimes[2], TRUE)
-})
+times <- data.frame(elapsedTimes, nbObjects)
+#plot(x=times$nbObjects, y=times$elapsedTimes)
 
-elapsedTimes <- createObjects(50)
-test_that("rJava is faster for the instantiation of 50 objects", {
-  expect_equal(elapsedTimes[1] < elapsedTimes[2], TRUE)
+fit <- lm(elapsedTimes ~ nbObjects, data=times)
+slope <- as.numeric(coef(fit)[2])
+
+test_that("Time to create one additional object", {
+  expect_equal(slope < 1E-4, TRUE)
 })
 
 shutdownJava()
