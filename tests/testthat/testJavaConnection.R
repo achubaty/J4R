7169f01bd0b5a6409f27b8e26e54b1c7ec2d3cdc
donenrique <donenrique@c5ebbc72-d003-4893-bbcb-3580462f3885>
2020-04-14 00:36:34 +0000

    Adding a test for the connection to another port

--- tests/testthat/testJavaConnection.R
+++ tests/testthat/testJavaConnection.R
@@ -29,7 +29,7 @@ test_that("The JVM has been properly shutted down by the shutdownJava function",
 #### Testing that two calls to connectToJava will not affect the socket connection ####
 
 callback <- connectToJava()
-test_that("Testing that the second call to connectToJava returns TRUE", {
+test_that("Testing if the second call to connectToJava returns TRUE", {
   expect_equal(callback, TRUE)
 })
 
@@ -47,7 +47,7 @@ jVersion <- getJavaVersion()
 versionOut <- jVersion$version
 architectureOut <- jVersion$architecture
 
-test_that("Testing that the getJavaVersion gives the same result whether or not the server is online", {
+test_that("Testing if the getJavaVersion gives the same result whether or not the server is online", {
   expect_equal(versionIn, versionOut)
   expect_equal(architectureIn, architectureOut)
 })
@@ -56,8 +56,18 @@ test_that("Testing that the getJavaVersion gives the same result whether or not 
 
 isConnected <- connectToJava(debug = T)
 
-test_that("Testing that the connectToJava version returns FALSE when it does not connect to the server", {
+test_that("Testing if the connectToJava function returns FALSE when it does not connect to the server", {
   expect_equal(isConnected, FALSE)
 })
 
+
+### Testing connection on another port ###
+
+isConnected <- connectToJava(port = 18013)
+
+test_that("Testing if the connectToJava function with another port works", {
+  expect_equal(isConnected, TRUE)
+})
+
+shutdownJava()
 
